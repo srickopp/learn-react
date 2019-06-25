40c291b9d48fae70ee45742b1ebae3cb1a1fa14e
Samuel Ricko <srickopp@gmail.com>
2019-06-25 14:50:53 +0700

    try to use react

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.          
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+  return(
+    <div>
+      <h1>Hello React</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto odit dolorem velit iusto! Veritatis delectus optio saepe nostrum earum! Est minus aut placeat, ducimus nobis dolorem vitae illum perspiciatis accusamus?</p>
     </div>
   );
 }
