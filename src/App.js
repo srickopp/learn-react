059a3c9c38b5457043ff92b0812dd1f34a42a06f
Samuel Ricko <srickopp@gmail.com>
2019-06-25 16:29:23 +0700

    setup base react

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.          
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-  return(
+  return (
     <div>
-      <h1>Hello React</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto odit dolorem velit iusto! Veritatis delectus optio saepe nostrum earum! Est minus aut placeat, ducimus nobis dolorem vitae illum perspiciatis accusamus?</p>
+      <h1>Rendering Elements</h1>
     </div>
   );
 }
