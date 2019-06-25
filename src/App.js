df215dec2270d9e03ad8474659588ebb07882adf
Samuel Ricko <srickopp@gmail.com>
2019-06-25 16:35:33 +0700

    render component from another funct

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import React from 'react';
 function App() {
   return (
     <div>
+      <Header />
       <h1>Rendering Elements</h1>
+    </div>
+  );
+}
+
+function Header(){
+  return(
+    <div>      
+      <h2>Makanan Khas</h2>
     </div>
   );
 }
