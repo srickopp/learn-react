a08184d025a1384d74e5dc1de9cee05bea471dc0
Samuel Ricko <srickopp@gmail.com>
2019-06-25 16:46:06 +0700

    create component from function

--- src/App.js
+++ src/App.js
@@ -6,8 +6,17 @@ function App() {
     <div>
       <Header />
       <h1>Rendering Elements</h1>
+      <Footer />
     </div>
   );
+}
+
+const Footer = () => {
+  return (
+    <div>
+      <h4>Ini Footer</h4>
+    </div>
+  )
 }
 
 export default App;
