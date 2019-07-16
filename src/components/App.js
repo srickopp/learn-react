ff1510206a061a6c5f43542f20814281101e0c15
Samuel Ricko <srickopp@gmail.com>
2019-07-16 22:15:59 +0700

    belajar setState

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,15 @@ import Header from './Header';
 import Footer from './Footer';
 import List from './List';
 import Top from './Top';
+import Main from './Main';
 
 function App() {
   return (
     <div>      
       <Header list="3 Terbaik"/>
       <Top />
-        <List />        
+      <Main />
+      <List />        
       <Footer namanya="Makanan Nusantara" tahun="2019"/>
     </div>
   );
---
+++ src/components/Main.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+class Main extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            title: "Menu Makanan",
+            title2: "Menu Minuman",
+            inputValue: "Nasi Padang Nusantara"
+        }
+
+        this.rubahData = this.rubahData.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    // rubahData(){
+    //     this.setState({title: "Pilih Makanan"});
+    // }
+
+    rubahData(){
+        this.setState((state,props) => {
+            return {
+                title: state.title2,
+                title2: state.title
+            }
+        })
+    }
+
+    handleChange(e){
+    
+        console.log(e.target.value);
+    }
+
+    render(){
+        return(
+            <div>
+                <h3>{this.state.title}</h3>
+                <h2>{this.state.title2}</h2>
+                <button onClick={this.rubahData}>Rubah Data</button>
+                <br/>
+                <br/>
+                <input
+                    type="text" 
+                    value={this.state.inputValue} 
+                    onChange={this.handleChange} 
+                />
+            </div>
+        )
+    }
+}
+
+export default Main;
